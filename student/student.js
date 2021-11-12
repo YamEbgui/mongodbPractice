fd5a2fdf7651b9682578754bf04bec564d376311
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-12 13:59:57 +0200

    Create schema for student

---
+++ student/student.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surName: {
+    type: String,
+    required: true,
+  },
+  birth: {
+    type: Date,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  courses: {
+    type: Array,
+    required: true,
+  },
+});
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Agent;
