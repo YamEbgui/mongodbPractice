6c67a8e1e949ac8026489dd7ea7dbe653773f944
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-13 17:58:27 +0200

    Add functions to search query in database

--- relationships/relationshipsQuery.js
+++ relationships/relationshipsQuery.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const mongoose = require("mongoose");
-const Student = require("./student");
+const User = require("./collections").User;
+const Post = require("./collections").Post;
+const Comment = require("./collections").Comment;
 const dotenv = require("dotenv").config();
 
 mongoose.connect(
@@ -14,5 +16,51 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully");
-  deleteAllStudentWiththisBirth();
+  returnCommentsForPost(postTitle);
 });
+
+function returnAllUsers() {
+  User.find({}).then((results) => {
+    console.log(results);
+  });
+}
+
+function returnAllPosts() {
+  Post.find({}).then((results) => {
+    console.log(results);
+  });
+}
+
+function returnAllGoodGuyPosts() {
+  Post.find({ username: "GoodGuyGreg" }).then((results) => {
+    console.log(results);
+  });
+}
+
+function returnAllScumbagStevePosts() {
+  Post.find({ username: "ScumbagSteve" }).then((results) => {
+    console.log(results);
+  });
+}
+
+function returnAllComments() {
+  Comment.find({}).then((results) => {
+    console.log(results);
+  });
+}
+
+function returnAllCommentsOfUser(user) {
+  Comment.find({ username: `${user}` }).then((results) => {
+    console.log(results);
+  });
+}
+
+function returnCommentsForPost(postTitle) {
+  Post.find({ title: `${postTitle}` }).then((results) => {
+    if (results.length > 0) {
+      Comment.find({ post: `${results[0]._id}` }).then((results) => {
+        console.log(results);
+      });
+    }
+  });
+}
